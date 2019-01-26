b75b5185d1054d056e48c3f1824df9efb7448adc
visualstudio <visualstudio@122-PC39>
2019-01-25 21:11:11 -0500

    reohj

--- c0730192/Program.cs
+++ c0730192/Program.cs
@@ -12,4 +12,39 @@ namespace c0730192
         {
         }
     }
+
+    class Countryside
+    {
+        Village Alst;
+        Village Maeland;
+        Village Schenig;
+
+        public void InitializeMap()
+        {
+            Alst = new Village();
+            Alst.VillageName = "alst';"
+            Maeland = new Village();
+            Maeland.isAstrildeHere = true;
+            Schenig = new Village();
+
+            Alst.NextVillage = Maeland;
+            Maeland.NextVillage = Schenig;
+            Schenig.NextVillage = Alst;
+            Schenig.VillageName = "schenig";
+        }
+
+        public void WalkAround()
+        {
+            Village InitialVillage = this.Maeland;
+        }
+    }
+
+    class Village
+    {
+        public Village PreviousVillage;
+        public Village NextVillage;
+        public String VillageName;
+        public bool isAstrildeHere = false;
+        public Village() { }
+    }
 }
